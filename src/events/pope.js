a503231d41f17ddaeb6f5bb654d44762ebbaa480
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-03 19:07:38 +0100

    Finished detecting 2137 in messages

--- src/events/pope.js
+++ src/events/pope.js
@@ -1,10 +1,47 @@
+import fs from "fs"
+
 export const name = "messageCreate"
 export const once = false
 export function execute(message) {
     let now = new Date()
+    let hours = now.getHours()
+    let minutes = now.getMinutes()
+    now = now.toISOString().split("T")[0]
 
-    if (message.content === "2137"
-        && now.getUTCHours() == 21 - 1 && now.getUTCMinutes() == 37) {
-        message.reply(`${message.author} to twoja x papieżowa, trzymaj kremówkę! 🍮`)
+    let yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1)
+    yesterday = yesterday.toISOString().split("T")[0]
+
+    let pope_list = JSON.parse(fs.readFileSync("src/logs/pope.json"))
+
+    if (message.content === "2137" && hours === 21 && minutes === 37) {
+        let entry = pope_list.find(e => e.id === message.author.id)
+
+        if (!entry) {
+            entry = {
+                id: message.author.id,
+                popes: 0,
+                popes_in_a_row: 0,
+                last_pope: now
+            }
+
+            pope_list.push(entry)
+        }
+
+        if (entry.last_pope !== now) {
+            entry.popes++
+            entry.last_pope === yesterday ? entry.popes_in_a_row++ : entry.popes_in_a_row = 0
+            entry.last_pope = now
+
+            let reply_message = `${message.author} to twoja ${entry.popes} papieżowa, `
+            if (entry.popes_in_a_row > 1) reply_message += `już ${entry.popes_in_a_row} z rzędu, `
+            reply_message += "trzymaj kremówkę! 🍮"
+
+            message.reply(reply_message)
+        } else {
+            message.reply(`${message.author} nieco za szybko piszesz tą godzine, może poczekaj do jutra co?`)
+        }
+
+        fs.writeFileSync("src/logs/pope.json", JSON.stringify(pope_list, null, 4))
     }
 }
