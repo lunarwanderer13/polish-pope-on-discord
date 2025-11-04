6f536584c0fbf7d0fb40cfe9f2cf98549550935d
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-04 22:05:30 +0100

    Made the pagination embed syntax clearer and easier to read

--- src/commands/barka.js
+++ src/commands/barka.js
@@ -6,65 +6,63 @@ export const data = new SlashCommandBuilder()
     .setDescription("Zobacz tekst ulubionej piosenki papierza")
 
 export async function execute(interaction) {
-    const pagination = new Pagination(interaction)
+    new Pagination(interaction)
+        .setTitle("🙏 Barka 🙏")
+        .setDescription("tłumaczenie przez ks. Stanisława Szmidta")
+        .setColor("#69bccd")
 
-    pagination.setTitle("🙏 Barka 🙏")
-    pagination.setDescription("tłumaczenie przez ks. Stanisława Szmidta")
-    pagination.setColor("#69bccd")
-
-    pagination.setFields([
-        {
-            name: "1. Pan kiedyś stanął nad brzegiem,",
-            value: `Szukał ludzi gotowych pójść za Nim;
-                By łowić serca
-                Słów Bożych prawdą.`
-        },
-        {
-            name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
-            value: `Twoje usta dziś wyrzekły me imię.
-                Swoją barkę pozostawiam na brzegu.
-                Razem z Tobą nowy zacznę dziś łów.`
-        },
-        {
-            name: "2. Jestem ubogim człowiekiem,",
-            value: `Moim skarbem są ręce gotowe
-                Do pracy z Tobą
-                I czyste serce.`
-        },
-        {
-            name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
-            value: `Twoje usta dziś wyrzekły me imię.
-                Swoją barkę pozostawiam na brzegu,
-                Razem z Tobą nowy zacznę dziś łów.`
-        },
-        {
-            name: "3. Ty, potrzebujesz mych dłoni,",
-            value: `Mego serca młodego zapałem
-                Mych kropli potu
-                I samotności.`
-        },
-        {
-            name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
-            value: `Twoje usta dziś wyrzekły me imię.
-                Swoją barkę pozostawiam na brzegu,
-                Razem z Tobą nowy zacznę dziś łów.`
-        },
-        {
-            name: "4. Dziś wypłyniemy już razem",
-            value: `Łowić serca na morzach dusz ludzkich
-                Twej prawdy siecią
-                I słowem życia.`
-        },
-        {
-            name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
-            value: `Twoje usta dziś wyrzekły me imię.
-                Swoją barkę pozostawiam na brzegu,
-                Razem z Tobą nowy zacznę dziś łów`
-        }
-    ])
-
-    pagination.paginateFields()
-    pagination.render()
+        .setFields([
+            {
+                name: "1. Pan kiedyś stanął nad brzegiem,",
+                value: `Szukał ludzi gotowych pójść za Nim;
+                    By łowić serca
+                    Słów Bożych prawdą.`
+            },
+            {
+                name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
+                value: `Twoje usta dziś wyrzekły me imię.
+                    Swoją barkę pozostawiam na brzegu.
+                    Razem z Tobą nowy zacznę dziś łów.`
+            },
+            {
+                name: "2. Jestem ubogim człowiekiem,",
+                value: `Moim skarbem są ręce gotowe
+                    Do pracy z Tobą
+                    I czyste serce.`
+            },
+            {
+                name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
+                value: `Twoje usta dziś wyrzekły me imię.
+                    Swoją barkę pozostawiam na brzegu,
+                    Razem z Tobą nowy zacznę dziś łów.`
+            },
+            {
+                name: "3. Ty, potrzebujesz mych dłoni,",
+                value: `Mego serca młodego zapałem
+                    Mych kropli potu
+                    I samotności.`
+            },
+            {
+                name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
+                value: `Twoje usta dziś wyrzekły me imię.
+                    Swoją barkę pozostawiam na brzegu,
+                    Razem z Tobą nowy zacznę dziś łów.`
+            },
+            {
+                name: "4. Dziś wypłyniemy już razem",
+                value: `Łowić serca na morzach dusz ludzkich
+                    Twej prawdy siecią
+                    I słowem życia.`
+            },
+            {
+                name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
+                value: `Twoje usta dziś wyrzekły me imię.
+                    Swoją barkę pozostawiam na brzegu,
+                    Razem z Tobą nowy zacznę dziś łów`
+            }
+        ])
+        .paginateFields()
+        .render()
 }
 
 /*
