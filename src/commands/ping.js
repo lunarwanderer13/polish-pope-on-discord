9abf2673abc456f2d510b35b23c4f6575076072d
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-04 23:01:06 +0100

    Added bot latency and made /ping ephemeral

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,9 +1,12 @@
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder, MessageFlags } from "discord.js"
 
 export const data = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!")
 
 export async function execute(interaction) {
-    await interaction.reply("Pong!")
+    await interaction.reply({
+        content: `Pong! *${interaction.createdTimestamp - Date.now()}ms*`,
+        flags: MessageFlags.Ephemeral
+    })
 }
