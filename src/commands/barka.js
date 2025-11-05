ee3b3b33f77d76d29f2587a90d4692654254e639
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-05 16:36:07 +0100

    Made barka.js's formatting better

--- src/commands/barka.js
+++ src/commands/barka.js
@@ -15,50 +15,50 @@ export async function execute(interaction) {
             {
                 name: "1. Pan kiedyś stanął nad brzegiem,",
                 value: `Szukał ludzi gotowych pójść za Nim;
-                    By łowić serca
-                    Słów Bożych prawdą.`
+                        By łowić serca
+                        Słów Bożych prawdą.`.replace(/^ +/gm, '')
             },
             {
                 name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
                 value: `Twoje usta dziś wyrzekły me imię.
-                    Swoją barkę pozostawiam na brzegu.
-                    Razem z Tobą nowy zacznę dziś łów.`
+                        Swoją barkę pozostawiam na brzegu.
+                        Razem z Tobą nowy zacznę dziś łów.`.replace(/^ +/gm, '')
             },
             {
                 name: "2. Jestem ubogim człowiekiem,",
                 value: `Moim skarbem są ręce gotowe
-                    Do pracy z Tobą
-                    I czyste serce.`
+                        Do pracy z Tobą
+                        I czyste serce.`.replace(/^ +/gm, '')
             },
             {
                 name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
                 value: `Twoje usta dziś wyrzekły me imię.
-                    Swoją barkę pozostawiam na brzegu,
-                    Razem z Tobą nowy zacznę dziś łów.`
+                        Swoją barkę pozostawiam na brzegu,
+                        Razem z Tobą nowy zacznę dziś łów.`.replace(/^ +/gm, '')
             },
             {
                 name: "3. Ty, potrzebujesz mych dłoni,",
                 value: `Mego serca młodego zapałem
-                    Mych kropli potu
-                    I samotności.`
+                        Mych kropli potu
+                        I samotności.`.replace(/^ +/gm, '')
             },
             {
                 name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
                 value: `Twoje usta dziś wyrzekły me imię.
-                    Swoją barkę pozostawiam na brzegu,
-                    Razem z Tobą nowy zacznę dziś łów.`
+                        Swoją barkę pozostawiam na brzegu,
+                        Razem z Tobą nowy zacznę dziś łów.`.replace(/^ +/gm, '')
             },
             {
                 name: "4. Dziś wypłyniemy już razem",
                 value: `Łowić serca na morzach dusz ludzkich
-                    Twej prawdy siecią
-                    I słowem życia.`
+                        Twej prawdy siecią
+                        I słowem życia.`.replace(/^ +/gm, '')
             },
             {
                 name: "Ref.: O Panie, to Ty na mnie spojrzałeś,",
                 value: `Twoje usta dziś wyrzekły me imię.
-                    Swoją barkę pozostawiam na brzegu,
-                    Razem z Tobą nowy zacznę dziś łów`
+                        Swoją barkę pozostawiam na brzegu,
+                        Razem z Tobą nowy zacznę dziś łów`.replace(/^ +/gm, '')
             }
         ])
 
