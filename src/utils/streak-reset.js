8f6dd404d4d3ff1ff930a3d915b926fb7d138c33
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-17 17:07:44 +0100

    Added a daily streak reset. Untested, will update whether it works or not in a few days.

---
+++ src/utils/streak-reset.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+const schedule = require("node-schedule")
+
+schedule.scheduleJob("30 21 * * *", () => {
+    let now = new Date()
+    now = now.toISOString().split("T")[0]
+    const data = JSON.parse(fs.readFileSync("src/logs/pope.json"))
+
+    for (user of data) {
+        if (user.last_pope !== now) {
+            user.popes_in_a_row = 0
+        }
+    }
+
+    fs.writeFileSync("src/logs/pope.json", JSON.stringify(data, null, 4))
+})
