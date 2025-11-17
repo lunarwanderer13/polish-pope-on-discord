59d60a57291c41e3ece42718a759e3b9eaba08a9
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-17 22:03:27 +0100

    Added logging and fixed a bug in the function

--- src/utils/streak-reset.js
+++ src/utils/streak-reset.js
@@ -1,12 +1,17 @@
 import fs from "fs"
 import schedule from "node-schedule"
 
+function log(level, message) {
+    const timestamp = new Date().toISOString()
+    console.log(`[${timestamp}] [${level}]: ${message}`)
+}
+
 schedule.scheduleJob("40 21 * * *", () => {
     let now = new Date()
     now = now.toISOString().split("T")[0]
     const data = JSON.parse(fs.readFileSync("src/logs/pope.json"))
 
-    for (user of data) {
+    for (const user of data) {
         if (user.last_pope !== now) {
             user.popes_in_a_row = 0
         }
